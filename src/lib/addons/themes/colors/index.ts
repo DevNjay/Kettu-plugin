0aac2c000f6f481f37e0961127a584f2db9a3b3e
Niño Jay Laurel <njaylaurel16@gmail.com>
2025-11-12 07:41:01 +0800

    Add files via upload

---
+++ src/lib/addons/themes/colors/index.ts
@@ -0,0 +1,23 @@
+import fixStatusBar from "./statusbar";
+import fixKeyboard from "./keyboard";
+import patchChatBackground from "./patches/background";
+import patchDefinitionAndResolver from "./patches/resolver";
+import patchStorage from "./patches/storage";
+import { ColorManifest } from "./types";
+import { updateBunnyColor } from "./updater";
+
+/** @internal */
+export default function initColors(manifest: ColorManifest | null) {
+    const patches = [
+        patchStorage(),
+        patchDefinitionAndResolver(),
+        patchChatBackground()
+    ];
+
+    fixStatusBar();
+    fixKeyboard();
+
+    if (manifest) updateBunnyColor(manifest, { update: false });
+
+    return () => patches.forEach(p => p());
+}
